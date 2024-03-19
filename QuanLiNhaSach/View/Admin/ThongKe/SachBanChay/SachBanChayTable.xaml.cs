958c452a4c699c96caf2151d554a2f6bcf386524
Dung Tien The <dungtienthe1920@gmail.com>
2024-03-19 13:23:32 +0700

    SachBanChay: xây dựng UI cho SachBanChayTable

---
+++ QuanLiNhaSach/View/Admin/ThongKe/SachBanChay/SachBanChayTable.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLiNhaSach.View.Admin.ThongKe.SachBanChay
+{
+    /// <summary>
+    /// Interaction logic for SachBanChayTable.xaml
+    /// </summary>
+    public partial class SachBanChayTable : Page
+    {
+        public SachBanChayTable()
+        {
+            InitializeComponent();
+        }
+    }
+}
