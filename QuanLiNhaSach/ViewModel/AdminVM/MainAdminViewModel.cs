9867fc23c8e7a0884d3d512ad55dcc030ea14518
Tran Thanh Dat <neban0444@gmail.com>
2024-05-07 16:51:22 +0700

    add nav screen

--- QuanLiNhaSach/ViewModel/AdminVM/MainAdminViewModel.cs
+++ QuanLiNhaSach/ViewModel/AdminVM/MainAdminViewModel.cs
@@ -16,6 +16,9 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Data.Entity;
+using QuanLiNhaSach.View.Admin.ProductsManager;
+using QuanLiNhaSach.View;
+using QuanLiNhaSach.View.Staff.Sale;
 
 namespace QuanLiNhaSach.ViewModel.AdminVM
 {
@@ -137,12 +140,17 @@ namespace QuanLiNhaSach.ViewModel.AdminVM
         public ICommand LoadSanPhamPage { get; set; }
         public ICommand LoadThongKePage { get; set; } 
         public ICommand LoadHeThongPage { get; set; }
+        public ICommand LoadBanHangPage { get; set; }
         public ICommand OpenAccountWindow { get; set; }
         public ICommand EditStaffCommand { get; set; }
         public ICommand OpenHelpPage { get; set; }
         public ICommand LogOutCommand { get; set; }
         public MainAdminViewModel() {
+            FirstLoadCM = new RelayCommand<Frame>((p) => { return true; }, (p) => { p.Content = new ProductMain(); });
             LoadKhachHangPage = new RelayCommand<Frame>((p) => { return true; }, (p) => { p.Content = new CustomerPage(); });
+            LoadSanPhamPage = new RelayCommand<Frame>((p) => { return true; }, (p)=> { p.Content = new ProductMain(); });
+            LoadHeThongPage = new RelayCommand<Frame>((p) => { return true; }, (p) => { p.Content = new SystemPage(); });
+            LoadBanHangPage = new RelayCommand<Frame>((p) => { return true; }, (p) => { p.Content = new SalePage(); });
             OpenAccountWindow = new RelayCommand<object>(null, (p) =>
             {
                 EditBirthDay = currentStaff.BirthDay.ToString();
