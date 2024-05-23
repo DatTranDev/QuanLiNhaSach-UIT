9f7d2ea9f2c9c42a9c6f5096f3330eb2c4d0557d
Dung Tien The <dungtienthe1920@gmail.com>
2024-05-23 22:47:45 +0700

    xay dung xuat cong no,ton kho ra file excel

--- QuanLiNhaSach/ViewModel/AdminVM/ThongKeVM/MotSoPTBoTro.cs
+++ QuanLiNhaSach/ViewModel/AdminVM/ThongKeVM/MotSoPTBoTro.cs
@@ -51,5 +51,19 @@ namespace QuanLiNhaSach.ViewModel.AdminVM.ThongKeVM
             }
             return false;
         }
+
+        public static string RandomFileName()
+        {
+            Random random = new Random();
+            int passwordLength = random.Next(10, 20);
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var password = new char[passwordLength];
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                password[i] = chars[random.Next(chars.Length)];
+            }
+            return new string(password);
+        }
     }
 }
